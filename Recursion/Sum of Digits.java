3c0aeb855876d3eac313c7aa28fca11c60cf91ba
Rahul Singh <rahul72310singh@gmail.com>
2021-07-23 23:17:42 +0530

    Create Sum of Digits.java

---
+++ Recursion/Sum of Digits.java
@@ -0,0 +1,43 @@
+/*
+Sum of digits (recursive)
+Send Feedback
+Write a recursive function that returns the sum of the digits of a given integer.
+Input format :
+Integer N
+Output format :
+Sum of digits of N
+Constraints :
+0 <= N <= 10^9
+Sample Input 1 :
+12345
+Sample Output 1 :
+15
+Sample Input 2 :
+9
+Sample Output 2 :
+9
+
+*/
+
+import java.util.Scanner;
+
+public class runner {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		System.out.println(solution.sumOfDigits(n));
+	}
+}
+
+
+
+public class solution {
+
+	public static int sumOfDigits(int input){
+        if(input==0){
+            return 0;
+        }
+        
+        return input%10+sumOfDigits(input/10);
+	}
+}
