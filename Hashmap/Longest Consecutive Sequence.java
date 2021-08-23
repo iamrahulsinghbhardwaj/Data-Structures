50caf543fa5d802650354a9a9529d301d7fde35d
Rahul Singh <rahul72310singh@gmail.com>
2021-08-23 17:37:33 +0530

    Create Longest Consecutive Sequence.java

---
+++ Hashmap/Longest Consecutive Sequence.java
@@ -0,0 +1,123 @@
+/*
+You are given an array of unique integers that contain numbers in random order. You have to find the longest possible sequence of consecutive numbers using the numbers from given array.
+You need to return the output array which contains starting and ending element. If the length of the longest possible sequence is one, then the output array must contain only single element.
+Note:
+1. Best solution takes O(n) time.
+2. If two sequences are of equal length, then return the sequence starting with the number whose occurrence is earlier in the array.
+Input format:
+The first line of input contains an integer, that denotes the value of the size of the array. Let us denote it with the symbol n.
+The following line contains n space separated integers, that denote the value of the elements of the array.
+Output format:
+The first and only line of output contains starting and ending element of the longest consecutive sequence. If the length of longest consecutive sequence, then just print the starting element.
+Constraints :
+0 <= n <= 10^6
+Time Limit: 1 sec
+Sample Input 1 :
+13
+2 12 9 16 10 5 3 20 25 11 1 8 6 
+Sample Output 1 :
+8 12 
+Sample Input 2 :
+7
+3 7 2 1 9 8 41
+Sample Output 2 :
+7 9
+Explanation: Sequence should be of consecutive numbers. Here we have 2 sequences with same length i.e. [1, 2, 3] and [7, 8, 9], but we should select [7, 8, 9] because the starting point of [7, 8, 9] comes first in input array and therefore, the output will be 7 9, as we have to print starting and ending element of the longest consecutive sequence.
+Sample Input 3 :
+7
+15 24 23 12 19 11 16
+Sample Output 3 :
+15 16
+
+*/
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+public class Runner {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static int[] takeInput() throws IOException {
+        int size = Integer.parseInt(br.readLine().trim());
+        int[] input = new int[size];
+
+        if (size == 0) {
+            return input;
+        }
+        
+		String[] strNums;
+		strNums = br.readLine().split("\\s");
+		
+        for (int i = 0; i < size; ++i) {
+            input[i] = Integer.parseInt(strNums[i]);
+        }
+
+        return input;
+    }
+
+    public static void printArray(ArrayList<Integer> arr) {
+        for (int element : arr) {
+            System.out.print(element + " ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+
+        int[] arr = takeInput();
+        ArrayList<Integer> ans = Solution.longestConsecutiveIncreasingSequence(arr);
+        printArray(ans);
+
+    }
+}
+
+  
+import java.util.ArrayList;
+import java.util.HashMap;
+public class Solution {
+	public static ArrayList<Integer> longestConsecutiveIncreasingSequence(int[] arr) {
+
+        HashMap<Integer,Boolean> map=new HashMap<>();
+        
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],true);
+        }
+        
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i]-1)){
+                map.put(arr[i],false);
+            }
+        }
+        
+        int startlen=0;
+        int endlen=0;
+        
+        for(int i=0;i<arr.length;i++){
+            if(map.get(arr[i])==true){
+               int templength=1;
+                
+               int tempstart=arr[i]; 
+                
+                while(map.containsKey(tempstart+templength)){
+                    templength++;
+                }
+                
+                if(templength>endlen){
+                    startlen=tempstart;
+                    endlen=templength;
+                }
+            }
+        }
+        
+        ArrayList<Integer> list=new ArrayList<>();
+        
+        list.add(startlen);
+        list.add(startlen+endlen-1);
+        
+        return list;
+	}
+}
