da083f6bcf2cdf02ea09e9840cf06c23c67976d4
Rahul Singh <rahul72310singh@gmail.com>
2021-08-25 16:19:39 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,1 +1,27 @@
 
+package Sorting;
+
+public class BubbleSort {
+	
+	public static void sort(int a[]) {
+		for(int i=0;i<a.length-1;i++) {
+			for(int j=i+1;j<a.length;j++) {
+				if(a[i]>a[j]) {
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+	}
+	
+	public static void main(String args[]) {
+		int a[]={1333,20,-3,4};
+		
+		sort(a);
+		
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]+" ");
+		}
+	}
+}
